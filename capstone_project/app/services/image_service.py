37fea2fabb3a473336495ed448bd1f9d8f9420ca
Joseph <josebonagain@gmail.com>
2025-12-23 23:32:57 +0300

    Add function to upload vehicle images to Cloudinary

--- capstone_project/app/services/image_service.py
+++ capstone_project/app/services/image_service.py
@@ -1,5 +1,5 @@
 import cloudinary
-
+import cloudinary.uploader
 import os
 
 cloudinary.config(
@@ -8,3 +8,16 @@ cloudinary.config(
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 
+def upload_vehicle_image(file_path, folder="matatu_images"):
+    try:
+        result = cloudinary.uploader.upload(
+            file_path,
+            folder=folder,
+            use_filename=True,
+            unique_filename=True,
+            overwrite=False
+        )
+        return result.get("secure_url")
+    except Exception as e:
+        print("Cloudinary Upload Error:", e)
+        return None
