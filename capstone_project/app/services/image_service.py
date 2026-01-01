97c0bce2c56c1d383235026024faecb32769ef26
Joseph <josebonagain@gmail.com>
2026-01-01 13:15:40 +0300

    configure Cloudinary and add vehicle image upload helper

---
+++ capstone_project/app/services/image_service.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import cloudinary
+import cloudinary.uploader
+import os
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+def upload_vehicle_image(file_path, folder="matatu_images"):
+    try:
+        result = cloudinary.uploader.upload(
+            file_path,
+            folder=folder,
+            use_filename=True,
+            unique_filename=True,
+            overwrite=False
+        )
+        return result.get("secure_url")
+    except Exception as e:
+        print("Cloudinary Upload Error:", e)
+        return None
